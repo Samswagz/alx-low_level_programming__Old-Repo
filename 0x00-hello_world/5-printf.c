c296c31b0592f3c7b059cda1292f33bde3cf3fdd
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-11 03:17:24 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,24 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - A programme that prints with printf
+ * main - A programme that prints printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - A programme that prints printf
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}#include <stdio.h>
+/**
+ * main - A programme that prints printf
  * Return: 0 (success)
  */
 int main(void)
