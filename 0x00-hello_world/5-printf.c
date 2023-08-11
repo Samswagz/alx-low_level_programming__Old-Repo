5b5b586b894bcaeb31f2e38b0684fd01161746cf
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-11 03:21:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,22 +7,4 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}#include <stdio.h>
-/**
- * main - A programme that prints printf
- * Return: 0 (success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}#include <stdio.h>
-/**
- * main - A programme that prints printf
- * Return: 0 (success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
 }
