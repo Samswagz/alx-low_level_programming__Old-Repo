0c73e2a86a74a685d77804fd1250527bfc3e1154
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-16 15:52:09 -0700

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,21 @@
 /**
  * _abs - A function that prints the absolute value of a number
  * @a: The integer whos absolute value is to be printed
- * Return: -a or a
+ * Return: abs_val or a
  */
 
 int _abs(int a)
 
 {
 	if (a < 0)
-		return (-1);
+	{
+		int abs_val;
 
+		abs_val = a * -1;
+		return (abs_val);
+	}
 	else
+	{
 		return (a);
+	}
 }
