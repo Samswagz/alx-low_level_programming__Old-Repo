ec49b17379cbb0779ad9a00cd66daeb5ee321687
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-16 20:07:51 -0700

    additional code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - A programme to add two numbers
+ * @x: the first integer
+ * @y: the second integer
+ * Return: x+y
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
