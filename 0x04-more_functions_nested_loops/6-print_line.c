b7b7160cb31a079b2eac6918def8dd717abde137
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-18 13:20:16 -0700

    ./6-lines | cat -e

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - A code that prints a stright line
+ * Return: always 0
+ * @n: the number of times _ is to be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
