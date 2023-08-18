448517c39b73da7418c9dc24b61757c1d037f60f
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-08-18 14:17:04 -0700

    indorama

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
- * print_line - A code that prints a stright line
- * Return: always 0
- * @n: the number of times _ is to be printed
+ * print_line -lines
+ * @n: number of times
+ * return: 0 for success
  */
+
 void print_line(int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
-	{
-		_putchar('_');
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
