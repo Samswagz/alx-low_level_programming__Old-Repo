10cc84ed621254db8cb30e1f625f20ca3666b162
ilesamuel53@gmail.com <ilesamuel53@gmail.com>
2023-09-01 16:09:25 -0700

    argument c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints number of arguments passed to it
+ * @argc: integer argument
+ * @argv: character argument
+ * Return: argc
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
